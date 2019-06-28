ed456c024751410f0db791ad4f2cef97c59a497d
Akos Kitta <kittaakos@typefox.io>
2019-06-28 13:09:02 +0200

    Simplified the structure. Updated README.
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- generate.sh
+++ generate.sh
@@ -9,4 +9,4 @@ export PATH=$PATH:$PWD/node_modules/.bin
 grpc_tools_node_protoc \
 --js_out=import_style=commonjs,binary:./src-gen/ \
 --grpc_out=./src-gen/ \
---plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` ./protos/*.proto
+--plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` ./*.proto
